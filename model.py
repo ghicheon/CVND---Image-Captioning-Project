242ade807139dbc8bb06783179a133e5a777d392
Ghicheon Lee <gebmania@gmail.com>
2018-06-17 22:46:39 +0900

    refactoring and more nodes

--- model.py
+++ model.py
@@ -24,7 +24,7 @@ class EncoderCNN(nn.Module):
     
 
 class DecoderRNN(nn.Module):
-    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=2):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
         super(DecoderRNN,self).__init__()
         self.embed = nn.Embedding(vocab_size,embed_size)
         self.lstm = nn.LSTM(embed_size , hidden_size, num_layers,batch_first=True)
@@ -33,36 +33,33 @@ class DecoderRNN(nn.Module):
     def forward(self,features,captions):
         arg1 = features.view(len(features),1,-1)
         arg2 = self.embed(captions[:,:-1])
-        #print("DEBUG1:", arg1.shape , arg2.shape)
+        #print("DEBUG1:", arg1.shape , arg2.shape) #DEBUG1: torch.Size([10, 1, 256]) torch.Size([10, 14, 256])
         in_ = torch.cat(( arg1,arg2),1)
-        #print("DEBUG2 ", in_.shape)
+        #print("DEBUG2 ", in_.shape) #DEBUG2  torch.Size([10, 15, 256])
         out,h = self.lstm(in_)
-        #print("DEBUG3 ",out.shape , h[0].shape)
+        #print("DEBUG3 ",out.shape , h[0].shape) #DEBUG3  torch.Size([10, 15, 512]) torch.Size([1, 10, 512])
         out = self.fc(out)
-        #print("DEBUG4 ",out.shape )
+        #print("DEBUG4 ",out.shape ) #DEBUG4  torch.Size([10, 15, 8856])
         return out
-    
 
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        #print("input.shape  ",  inputs.shape)   # 1 1 256
-        #print("len(inputs)  ", len(inputs))
+        #print("input.shape  ",  inputs.shape)   # 1 1 512
   
         ret=[]
         for i in range(max_len):
            out,states = self.lstm(inputs,states)
-           #print("sample1:", out.shape)  # 1 1 512
+           #print("sample1:", out.shape)  # 1 1 1024
            out = self.fc(out.squeeze(1))
            #print("sample2:", out.shape)   # 1 8855
             
            idx = out.max(1)[1]
-           #print("sample3:",idx, idx.shape)   # [0]   
+           #print("sample3:",idx, idx.shape)  
             
            ret.append(idx.item())
            
            inputs = self.embed(idx).unsqueeze(1)   
-           #print("inputs.shape",inputs.shape)
   
         return ret
 
